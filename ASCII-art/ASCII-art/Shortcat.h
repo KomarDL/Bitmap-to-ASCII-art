c47e2333b790e67506cc4863f43fe3a070a29c66
KomarDL <deagl_Dim@mail.ru>
2019-12-19 00:06:09 +0300

    Add: accelerator resource

---
+++ ASCII-art/ASCII-art/Shortcat.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Shortcat.rc
+//
+#define IDR_ACCELERATOR                 101
+#define ID_ACCELERATOR40001             40001
+#define ID_ACCELERATOR40002             40002
+#define ID_ACCELERATOR40003             40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
