72d9a6482ce428003c08cefbd4fae2560a377635
KomarDL <deagl_Dim@mail.ru>
2019-12-19 00:04:01 +0300

    Add: menu resource

---
+++ ASCII-art/ASCII-art/Menu.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется ASCII-art.rc
+//
+#define IDR_MENU							101
+#define ID_MENU_OPEN                        40001
+#define ID_MENU_HELP                        40002
+#define ID_MENU_EXIT                        40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
